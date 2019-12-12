bb1219898ae3e4eab2683f05ab503c0086213ee3
Zhihao Jia <zhihao@cs.stanford.edu>
2019-12-12 07:02:54 +0000

    [Runtime] bug fixes for Merge group convolutions

--- src/cudnn/merge_gconv_kernel.cu
+++ src/cudnn/merge_gconv_kernel.cu
@@ -17,6 +17,24 @@
 #include "taso/cuda_helper.h"
 using namespace taso;
 
+__global__
+void merge_gconv_kernel(DATATYPE* dst_ptr,
+                        const DATATYPE* src_ptr,
+                        int volume,
+                        int c_in_h_w,
+                        int c_out,
+                        int count)
+{
+  assert(c_out % count == 0);
+  CUDA_KERNEL_LOOP(i, volume)
+  {
+    int mod = i % c_in_h_w;
+    int div = i / c_in_h_w;
+    int dst_i = div * c_in_h_w * count + div / (c_out / count) * c_in_h_w + mod;
+    dst_ptr[dst_i] = src_ptr[i];
+  }
+}
+
 void MergeGConv::map(void)
 {
   size_t outputSize = sizeof(DATATYPE) * outputs[0].volume();
@@ -30,9 +48,16 @@ void MergeGConv::unmap(void)
 
 void MergeGConv::forward(bool block)
 {
-  //merge_gconv_kernel<<<GET_BLOCKS(outputs[0].volume()), CUDA_NUM_THREADS>>>(
-  //    (DATATYPE*)outputs[0].data_ptr, (DATATYPE*)inputs[0].data_ptr,
-      
+  int c_out = inputs[0].dim[0];
+  int c_in_h_w = inputs[0].volume() / c_out;
+  assert(outputs[0].dim[1] % inputs[0].dim[1] == 0);
+  int count = outputs[0].dim[1] / inputs[0].dim[1];
+  assign_kernel<<<GET_BLOCKS(outputs[0].volume()), CUDA_NUM_THREADS>>>(
+      (DATATYPE*)outputs[0].data_ptr, outputs[0].volume(), 0.0f);
+  merge_gconv_kernel<<<GET_BLOCKS(inputs[0].volume()), CUDA_NUM_THREADS>>>(
+      (DATATYPE*)outputs[0].data_ptr, (DATATYPE*)inputs[0].data_ptr,
+      inputs[0].volume(), c_in_h_w, c_out, count);
+
   if (block)
     checkCUDA(cudaDeviceSynchronize());
 }
