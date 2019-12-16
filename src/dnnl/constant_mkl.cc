fc0a5c30f25cbaa41723ed7b755f3aa36f8e8a38
Mingyu Gao <gaomy@tsinghua.edu.cn>
2019-12-16 17:06:31 +0800

    [DNNL] add Conv2D and Constant.

---
+++ src/dnnl/constant_mkl.cc
@@ -0,0 +1,37 @@
+/* Copyright 2019 Stanford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "taso/ops.h"
+#include "taso/dnnl_helper.h"
+using namespace taso;
+
+void Constant::map(void)
+{
+  // allocate tensors
+  size_t outputSize = sizeof(DATATYPE) * outputs[0].volume();
+  CHECK_NE(nullptr, outputs[0].data_ptr = malloc(outputSize));
+}
+
+void Constant::unmap(void)
+{
+  // free tensors
+  free(outputs[0].data_ptr);
+  outputs[0].data_ptr = nullptr;
+}
+
+void Constant::forward(bool block)
+{
+}
+
