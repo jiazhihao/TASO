3f4f985235688976b1bde7b0454859f62592ac78
Mingyu Gao <gaomy@tsinghua.edu.cn>
2019-12-26 22:42:35 +0800

    [DNNL] add unimplemented operations: Mul, Pad, Reduce, Resize, Slice, TopK, Where.

---
+++ src/dnnl/topk_mkl.cc
@@ -0,0 +1,46 @@
+/* Copyright 2019 Stanford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "taso/ops.h"
+#include "taso/dnnl_helper.h"
+using namespace taso;
+using namespace dnnl;
+
+void TopK::map(void)
+{
+  // allocate tensors
+  size_t outputSize = sizeof(DATATYPE) * outputs[0].volume();
+  CHECK_NE(nullptr, outputs[0].data_ptr = malloc(outputSize));
+}
+
+void TopK::unmap(void)
+{
+  // clear primitives
+  net.clear();
+  // free tensors
+  free(outputs[0].data_ptr);
+  outputs[0].data_ptr = nullptr;
+}
+
+void TopK::forward(bool block)
+{
+  assert(false);
+}
+
+void Model::measure_topk_cost(TopK* topk)
+{
+  assert(false);
+}
+
